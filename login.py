323891ae0c73bb88485d86c652076c0d1de895d4
Jonathan Davey <Jonnyd456@hotmail.com>
2022-10-05 22:10:09 +0100

    Update login.py
    I merged the function that's called when you click register, to first check whether the user is on the login screen, or the register screen, and if it's on the registration screen already, add the inputted details into the database. However, if this was a real application, there would need to be some feedback for the user to know something actually happened. Text simply disappearing off the screen would have this effect, but better yet, text thanking the user for registering their details.

--- login.py
+++ login.py
@@ -10,10 +10,11 @@ HEADER_FONT = ("Calibri", 24, "bold")
 SMALLER_FONT = ("Calibri", 12, "bold")
 EVEN_SMALLER_FONT = ("Calibri", 9)
 
-
 class LoginScreen:
     def __init__(self):
         super().__init__()
+
+        self.state = 'login'
 
         self.image_frame = customtkinter.CTkFrame(width=643, height=429)
         self.image_frame.pack()
@@ -25,34 +26,34 @@ class LoginScreen:
         )
         self.image_label.place(anchor="center", x=643//2, y=429//2)
 
-        self.login_frame = customtkinter.CTkFrame()
-        self.login_frame.place(anchor="nw", height=214, width=643, x=0, y=429)
+        self.fields_container = customtkinter.CTkFrame()
+        self.fields_container.place(anchor="nw", height=214, width=643, x=0, y=429)
 
         self.login_header = customtkinter.CTkLabel(
-            self.login_frame, text="Login", text_font=HEADER_FONT, padx=0, pady=0
+            self.fields_container, text="Login", text_font=HEADER_FONT, padx=0, pady=0
         )
         self.login_header.place(anchor="center", x=322, y=29)
 
         self.login_username = customtkinter.CTkLabel(
-            self.login_frame, anchor="w", text="Username: ", text_font=SMALLER_FONT
+            self.fields_container, anchor="w", text="Username: ", text_font=SMALLER_FONT
         )
         self.login_username.place(anchor="center", x=205, y=76)
 
         self.login_password = customtkinter.CTkLabel(
-            self.login_frame, anchor="w", text="Password: ", text_font=SMALLER_FONT
+            self.fields_container, anchor="w", text="Password: ", text_font=SMALLER_FONT
         )
         self.login_password.place(anchor="center", x=204, y=124)
 
-        self.login_entry = customtkinter.CTkEntry(self.login_frame, width=230)
+        self.login_entry = customtkinter.CTkEntry(self.fields_container, width=230)
         self.login_entry.place(anchor="center", x=373, y=75)
 
         self.password_entry = customtkinter.CTkEntry(
-            self.login_frame, width=230, show="*"
+            self.fields_container, width=230, show="*"
         )
         self.password_entry.place(anchor="center", x=373, y=125)
 
         self.login_button = customtkinter.CTkButton(
-            self.login_frame,
+            self.fields_container,
             text="Login",
             text_font=SMALLER_FONT,
             corner_radius=30,
@@ -62,18 +63,38 @@ class LoginScreen:
         self.login_button.place(anchor="center", x=249, y=175)
 
         self.register_button = customtkinter.CTkButton(
-            self.login_frame,
+            self.fields_container,
             text="Register",
             text_font=SMALLER_FONT,
             corner_radius=30,
             width=120,
+            command=self.register_button
         )
         self.register_button.place(anchor="center", x=395, y=175)
 
         # Validation Text
         self.validation_text = customtkinter.CTkLabel(
-            self.login_frame, text="", text_font=EVEN_SMALLER_FONT)
+            self.fields_container, text="", text_font=EVEN_SMALLER_FONT)
         self.validation_text.place(anchor="center", x=545, y=175)
+
+    def register_button(self):
+        if self.state == 'register':
+            conn = sqlite3.connect('customers.db')
+            c = conn.cursor()
+
+            new_details = [
+                (str(self.login_entry.get()), str(self.password_entry.get()))
+            ]
+
+            c.executemany("INSERT INTO customers VALUES (?, ?)", new_details)
+            print(c.fetchall())
+
+            conn.commit()
+            conn.close()
+            conn.close()
+        else:
+            self.login_header.configure(text="Register")
+            self.state = 'register'
 
     def validate_database(self):
         conn = sqlite3.connect('customers.db')

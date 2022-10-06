097664f28b7df4eee970623fef9522cf8d05cfe8
Jonathan Davey <Jonnyd456@hotmail.com>
2022-10-06 02:51:24 +0100

    Add files via upload
    Adding a separate file to keep settings for the UI, in my attempt to make my code look tidier

---
+++ ui.py
@@ -0,0 +1,13 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("green")
+
+class Fonts:
+    HEADER_FONT = ("Calibri", 24, "bold")
+    SMALLER_FONT = ("Calibri", 12, "bold")
+    EVEN_SMALLER_FONT = ("Calibri", 9)
+
+class Colours:
+    SUCCESS = '#118408'
+    FAILURE = '#7c0000'
